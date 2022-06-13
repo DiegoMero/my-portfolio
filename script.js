db4dcf7e4a8fbc95ecf1a0af6a5ca3426f515e97
Diego Mero <diegomero_1999@hotmail.com>
2022-06-13 17:47:02 -0500

    Add css styles to javascript menu

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-let hamburger = document.querySelector('.menu');
-
+const hamburger = document.querySelector('.menu');
 hamburger.addEventListener('click', displaymenu);
+const body = document.querySelector('body');
 
-let body = document.querySelector('body');
-
-
-
-function displaymenu(){
-
-  let menu = document.createElement('div');
+function displaymenu() {
+  const menu = document.createElement('div');
   menu.className = 'menu-attributes';
+  const iconX = document.createElement('img');
 
-  let iconX = document.createElement('img');
-  iconX.src='./images/Iconx.png';
-  iconX.addEventListener('click', ()=>{body.removeChild(menu)});
+  iconX.src = './images/Iconx.png';
+  iconX.className = 'xicon';
+  iconX.addEventListener('click', () => { body.removeChild(menu); });
 
-  let portfolio = document.createElement('a');
-  portfolio.href= '#works-section';
-  portfolio.textContent = "Portfolio";
-  portfolio.addEventListener('click', ()=>{body.removeChild(menu)});
+  const portfolio = document.createElement('a');
+  portfolio.href = '#works-section';
+  portfolio.textContent = 'Portfolio';
+  portfolio.className = 'menu-text-style';
+  portfolio.addEventListener('click', () => { body.removeChild(menu); }); 
 
-  let aboutme = document.createElement('a');
-  aboutme.href= '#about-me-section';
-  aboutme.textContent = "About";
-  aboutme.addEventListener('click', ()=>{body.removeChild(menu)});
+  const aboutme = document.createElement('a');
+  aboutme.href = '#about-me-section';
+  aboutme.textContent = 'About';
+  aboutme.className = 'menu-text-style';
+  aboutme.addEventListener('click', () => { body.removeChild(menu); });
 
-  let contact = document.createElement('a');
-  contact.href= '#contact-me-section';
-  contact.textContent = "Contact";
-  contact.addEventListener('click', ()=>{body.removeChild(menu)});
-  
+  const contact = document.createElement('a');
+  contact.href = '#contact-me-section';
+  contact.textContent = 'Contact';
+  contact.className = 'menu-text-style';
+  contact.addEventListener('click', () => { body.removeChild(menu); });
+
   menu.appendChild(iconX);
   menu.appendChild(portfolio);
   menu.appendChild(aboutme);
   menu.appendChild(contact);
-
   body.appendChild(menu);
 }
-
-console.log(hamburger)
