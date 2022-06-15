d6b5f9baac215e32b6071ed80691d5cacf715e05
Diego Mero <diegomero_1999@hotmail.com>
2022-06-15 15:57:07 -0500

    Add form validation with javascript

--- script.js
+++ script.js
@@ -273,3 +273,76 @@ function displaymodal(e) {
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', displaymodal);
 }
+
+function showMessage(input, message, type) {
+  // get the small element and set the message
+  const msg = document.querySelector('small');
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? 'success' : 'error';
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+
+function isUpper(str) {
+  return !/^[a-z]*$/.test(str) && /[A-Z]/.test(str);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const email = input.value.trim();
+
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+function checkUppercase(input, invalidMsg) {
+  const email = input.value.trim();
+  if (isUpper(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+const form = document.querySelector('form');
+
+const lowerCaseCheck = 'Email needs to be in lower case';
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const EMAIL_INVALID = 'Please enter a correct email address format';
+
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
+
+  // validate the form
+  hasValue(form.elements["name"], NAME_REQUIRED);
+  validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+  checkUppercase(form.elements["email"], lowerCaseCheck);
+  // if valid, submit the form.
+  /* if (nameValid && emailValid && emailCheck) {
+    alert('Demo only. No form was posted.');
+  } */
+});
