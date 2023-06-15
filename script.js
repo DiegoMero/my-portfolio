0a22527c45662b229704bec03ab5e76908d2db47
Diego Mero <diegomero_1999@hotmail.com>
2023-06-15 02:39:45 -0500

    Add card project styling

--- script.js
+++ script.js
@@ -51,6 +51,85 @@ function displaymenu() {
 
 menuButton.addEventListener('click', displaymenu);
 
+function displayProjectCard() {
+  const projectCard = document.createElement('div');
+  projectCard.className = 'popup-card';
+  body.appendChild(projectCard);
+
+  const projectHeader = document.createElement('div');
+  projectCard.appendChild(projectHeader);
+
+  const projectTitle = document.createElement('h2');
+  projectTitle.textContent = 'Super Mario Run';
+  projectHeader.appendChild(projectTitle);
+
+  const iconX = document.createElement('img');
+  iconX.src = './images/IconXgrey.png';
+  iconX.addEventListener('click', () => {
+    body.removeChild(projectCard);
+    document.body.style.overflow = 'auto';
+  });
+  projectHeader.appendChild(iconX);
+
+  const projectPic = document.createElement('img');
+  projectPic.src = 'images/super-mario-run-mobile.png';
+  projectPic.className = 'project-card-pic-mobile';
+  projectCard.appendChild(projectPic);
+
+  const projectDescription = document.createElement('h3');
+  projectDescription.textContent = 'This web page represents a run in the Mario Bros universe.';
+  projectCard.appendChild(projectDescription);
+
+  const projectTools = document.createElement('ul');
+  projectCard.appendChild(projectTools);
+
+  const projectTool1 = document.createElement('li');
+  projectTools.appendChild(projectTool1);
+  projectTool1.textContent = 'HTML';
+
+  const projectTool2 = document.createElement('li');
+  projectTools.appendChild(projectTool2);
+  projectTool2.textContent = 'CSS';
+
+  const projectTool3 = document.createElement('li');
+  projectTools.appendChild(projectTool3);
+  projectTool3.textContent = 'JavaScript';
+
+  const span = document.createElement('span');
+  projectCard.appendChild(span);
+
+  const buttons = document.createElement('div');
+  projectCard.appendChild(buttons);
+  buttons.className = 'buttons-container';
+
+  const seeProjectButton = document.createElement('button');
+  buttons.appendChild(seeProjectButton);
+
+  const seeProjectText = document.createElement('h4');
+  seeProjectText.textContent = 'See live';
+  seeProjectButton.appendChild(seeProjectText);
+
+  const seeProjectIcon = document.createElement('i');
+  seeProjectIcon.className = 'fa-solid fa-paper-plane';
+  seeProjectButton.appendChild(seeProjectIcon);
+
+  const seeSourceButton = document.createElement('button');
+  buttons.appendChild(seeSourceButton);
+
+  const seeSourceText = document.createElement('h4');
+  seeSourceText.textContent = 'See source';
+  seeSourceButton.appendChild(seeSourceText);
+
+  const seeSourceIcon = document.createElement('i');
+  seeSourceIcon.className = 'fa-brands fa-github';
+  seeSourceButton.appendChild(seeSourceIcon);
+
+  document.body.style.overflow = 'hidden';
+}
+
+const botones = document.querySelector('.project-button');
+botones.addEventListener('click', displayProjectCard);
+
 const titulos = document.querySelectorAll('.skill-title');
 
 titulos.forEach((titulo) => {
